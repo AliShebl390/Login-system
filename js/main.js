1ebd2cafc989133c65e52ddb547a527f250f41d4
Ali Shebl <80364389+AliShebl390@users.noreply.github.com>
2023-12-25 18:16:01 +0200

    Finshed

---
+++ js/main.js
@@ -0,0 +1,64 @@
+let userName = document.querySelector("#userName");
+let userPassword = document.querySelector("#userPassword");
+let userEmail = document.querySelector("#userEmail");
+let logEmail = document.querySelector("#logEmail");
+let logPassword = document.querySelector("#logPassword");
+let regBtn = document.querySelector("#register");
+let signBtn = document.querySelector("#login");
+let signOut = document.querySelector("#signOut");
+let currentUser = [];
+let users = [];
+if (localStorage.getItem("userData") !== null) {
+    users = JSON.parse(localStorage.getItem("userData"));
+}
+if (localStorage.getItem("currentUser") !== null) {
+    currentUser = JSON.parse(localStorage.getItem("currentUser"));
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    if (localStorage.getItem("currentUser") === null) {
+        if (
+            window.location.href == "http://127.0.0.1:5500/login.html" ||
+            window.location.href == "http://127.0.0.1:5500/index.html"
+        ) {
+            return;
+        }
+        location.href = "http://127.0.0.1:5500/login.html";
+    }
+});
+
+function addUser() {
+    let user = {
+        name: userName.value,
+        password: userPassword.value,
+        email: userEmail.value,
+    };
+    users.push(user);
+    localStorage.setItem("userData", JSON.stringify(users));
+    location.href = "http://127.0.0.1:5500/login.html";
+}
+
+function logUser() {
+    for (let i = 0; i < users.length; i++) {
+        if (
+            logEmail.value === users[i].email &&
+            logPassword.value === users[i].password
+        ) {
+            let currentUser = {
+                email: logEmail.value,
+                password: logPassword.value,
+                name: users[i].name,
+            };
+            localStorage.setItem("currentUser", JSON.stringify(currentUser));
+            localStorage.setItem("userData", JSON.stringify(users));
+            location.href = "http://127.0.0.1:5500/home.html";
+            return;
+        }
+    }
+}
+
+function logOut() {
+    localStorage.removeItem("currentUser");
+    location.href = "http://127.0.0.1:5500/login.html";
+    return;
+}
