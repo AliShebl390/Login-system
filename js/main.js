de4f768bb5bd6113784e14df3f986124da45120e
Ali Shebl <80364389+AliShebl390@users.noreply.github.com>
2023-12-26 15:41:37 +0200

    Create validation for regitration form

--- js/main.js
+++ js/main.js
@@ -17,14 +17,18 @@ if (localStorage.getItem("currentUser") !== null) {
 
 
 function addUser() {
-    let user = {
-        name: userName.value,
-        password: userPassword.value,
-        email: userEmail.value,
-    };
-    users.push(user);
-    localStorage.setItem("userData", JSON.stringify(users));
-    location.href = "login.html";
+    if (validateregName() && validateregEmail() && validateregPass()) {
+        let user = {
+            name: userName.value,
+            password: userPassword.value,
+            email: userEmail.value,
+        };
+        users.push(user);
+        localStorage.setItem("userData", JSON.stringify(users));
+        location.href = "login.html";
+    } else {
+        regAlert.show()
+    }
 }
 
 function logUser() {
@@ -52,3 +56,69 @@ function logOut() {
     return;
 }
 
+function validateregName() {
+    let regNameRegax = /^[a-zA-Z0-9_]{3,16}$/
+    if (regNameRegax.test(userName.value)) {
+        userName.classList.add("is-valid")
+        userName.classList.remove("is-invalid")
+        return true
+    } else {
+        userName.classList.remove("is-valid")
+        userName.classList.add("is-invalid")
+        return false
+    }
+}
+
+function validateregEmail() {
+    let regEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+    if (regEmail.test(userEmail.value)) {
+        userEmail.classList.add("is-valid")
+        userEmail.classList.remove("is-invalid")
+        return true
+    } else {
+        userEmail.classList.remove("is-valid")
+        userEmail.classList.add("is-invalid")
+        return false
+    }
+}
+
+function validateregPass() {
+    let regPassRegax = /^.{6,}$/
+    if (regPassRegax.test(userPassword.value)) {
+        userPassword.classList.add("is-valid")
+        userPassword.classList.remove("is-invalid")
+        return true
+    } else {
+        userPassword.classList.remove("is-valid")
+        userPassword.classList.add("is-invalid")
+        return false
+    }
+}
+
+// logPassword.addEventListener("input",validateSignPass)
+// function validateSignPass() {
+//     let LogPassRegax = /^.{6,}$/
+//     if (LogPassRegax.test(logPassword.value)) {
+//         logPassword.classList.add("is-valid")
+//         logPassword.classList.remove("is-invalid")
+//         return true
+//     } else {
+//         logPassword.classList.remove("is-valid")
+//         logPassword.classList.add("is-invalid")
+//         return false
+//     }
+// }
+
+// logEmail.addEventListener("input",validateSignEmail)
+// function validateSignEmail() {
+//     let LogEmailRegax = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+//     if (LogEmailRegax.test(logEmail.value)) {
+//         logEmail.classList.add("is-valid")
+//         logEmail.classList.remove("is-invalid")
+//         return true
+//     } else {
+//         logEmail.classList.remove("is-valid")
+//         logEmail.classList.add("is-invalid")
+//         return false
+//     }
+// }
